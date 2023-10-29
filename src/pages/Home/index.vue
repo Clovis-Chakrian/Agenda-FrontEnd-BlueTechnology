ee9fd9c5f2d6748b49237b85ef250bafa85eb223
Clóvis Chakrian <clovischakriandev@gmail.com>
2023-10-29 19:36:09 -0300

    Feat: added button to navigate to create a contact and added a contact card

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,3 +1,33 @@
 <template>
-  <h1>Hello World</h1>
+  <main>
+    <ContactCard />
+  </main>
+  <Button
+    icon="pi pi-plus"
+    rounded
+    aria-label="adicionar contato"
+    class="btn-add-contact"
+  />
 </template>
+
+<script>
+import ContactCard from '../../components/ContactCard/index.vue';
+export default {
+  components: {
+    ContactCard
+  },
+  setup() {
+    
+  },
+}
+</script>
+
+<style scoped>
+.btn-add-contact {
+  width: 60px;
+  height: 60px;
+  position: absolute;
+  top: calc(100% - 70px);
+  left: calc(100% - 70px);
+}
+</style>
