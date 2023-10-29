02ae34e357417d198aeda93c15f9e4db5da175b5
Clóvis Chakrian <clovischakriandev@gmail.com>
2023-10-29 19:36:52 -0300

    Feat: made a card to present contacts at home screen

---
+++ src/components/ContactCard/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="contact-card">
+    <div class="contact-card-info">
+      <Avatar
+        label="CC"
+        size="large"
+        style="background-color: #2196f3; color: #ffffff"
+        shape="circle"
+      />
+      <p class="contact-name">Clóvis Chakrian</p>
+      <Button size="small" text raised icon="pi pi-phone" aria-label="ligar" />
+      <Button
+        size="small"
+        text
+        raised
+        icon="pi pi-pencil"
+        aria-label="editar contato"
+      />
+    </div>
+    <Divider />
+  </div>
+</template>
+
+<style scoped>
+.contact-card {
+  display: flex;
+  flex-direction: column;
+}
+
+.contact-card-info {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.contact-name {
+  width: 25%;
+}
+</style>
