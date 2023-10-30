4a9a1d51467368d56a9a26a8cc9403ffde27726e
Clóvis Chakrian <clovischakriandev@gmail.com>
2023-10-30 07:26:29 -0300

    Feat(CreateContact): made the basic html and css for the page

--- src/pages/CreateContact/index.vue
+++ src/pages/CreateContact/index.vue
@@ -1,3 +1,24 @@
 <template>
-  
+  <header>
+    <a href="">Voltar</a>
+  </header>
+  <main class="main">
+    <Avatar
+      label="CC"
+      size="xlarge"
+      style="background-color: #2196f3; color: #ffffff"
+      shape="circle"
+    />
+    <form class="form">
+      <InputText placeholder="John" />
+      <InputText placeholder="Doe" />
+      <InputMask
+        id="basic"
+        mask="(99) 99999-9999"
+        placeholder="(81) 99999-9999"
+      />
+      <InputText placeholder="email@exemplo.com" />
+      <Button label="Adicionar contato" />
+    </form>
+  </main>
 </template>
