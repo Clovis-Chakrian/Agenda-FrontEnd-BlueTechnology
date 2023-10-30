8a6f7a9d5691333d1927c3af5ef9d63322266ae2
Clóvis Chakrian <clovischakriandev@gmail.com>
2023-10-30 18:18:40 -0300

    Feat: added confirm dialog component and confirmation service from vueprime

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import ToastService from 'primevue/toastservice';
+import ConfirmationService from 'primevue/confirmationservice';
 
 import 'primevue/resources/themes/lara-light-teal/theme.css';
 import 'primeicons/primeicons.css';
@@ -12,10 +13,12 @@ import Divider from 'primevue/divider';
 import InputText from 'primevue/inputtext';
 import InputMask from 'primevue/inputmask';
 import Toast from 'primevue/toast';
+import ConfirmDialog from 'primevue/confirmdialog';
 
 const app = createApp(App);
 app.use(PrimeVue);
 app.use(ToastService);
+app.use(ConfirmationService);
 app.use(router);
 app.mount('#app');
 
@@ -24,4 +27,5 @@ app.component('Button', Button);
 app.component('Divider', Divider);
 app.component('InputText', InputText);
 app.component('InputMask', InputMask);
-app.component('Toast', Toast)
+app.component('Toast', Toast);
+app.component('ConfirmDialog', ConfirmDialog);
