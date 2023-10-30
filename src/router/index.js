f6dd70e55e59cf25fd0db5d853e265912bd62620
Clóvis Chakrian <clovischakriandev@gmail.com>
2023-10-30 07:26:52 -0300

    Feat: added route for create contact page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../pages/Home';
 import EditContact from '../pages/EditContact';
+import CreateContact from '../pages/CreateContact';
 
 const routes = [
   {
@@ -12,6 +13,11 @@ const routes = [
     path: '/edit',
     name: 'edit',
     component: EditContact
+  },
+  {
+    path: '/create-contact',
+    name: 'create',
+    component: CreateContact
   }
 ];
 
