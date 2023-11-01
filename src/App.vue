5221741ba338415b8a7b4091785f9252a901cd78
Clóvis Chakrian <clovischakriandev@gmail.com>
2023-11-01 08:25:17 -0300

    Feat: added a header to the project

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,29 @@
 <template>
-  <!-- <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav> -->
-  <router-view/>
+  <div class="content">
+    <header>
+      <h1>Minha Agenda</h1>
+    </header>
+    <router-view />
+  </div>
 </template>
 
-<style>
+<style scoped>
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+header {
+  margin: 0px;
+  padding: 0px;
+  width: 100%;
+  height: 70px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 20px;
+  border-bottom: 1px solid #62bb82;
+}
 </style>
